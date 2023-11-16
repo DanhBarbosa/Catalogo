2b8fe391306dd2b70d80e5e6f057ed15a0ebaf6c
Danilo Barbosa <101227933+DanhBarbosa@users.noreply.github.com>
2023-11-16 17:28:45 -0300

    create arquivos

---
+++ Catálogo Livros/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <nav id="navbar">
+        <h2>
+          <link to="/">Catalogo Livros</link>
+        </h2>
+        <link to="/movie/1">Livros</link>
+        <link to="/search">Search</link>
+      </nav>
+      <h2>Catálogo de Livros</h2>
+    </div>
+  )
+}
+
+export default App
